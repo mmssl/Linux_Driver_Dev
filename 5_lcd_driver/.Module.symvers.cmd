a562bf2262b44abde97a13fcff061e9bdda7ad61
muharrem.suslu <muharrem.suslu@aspilsan.com>
2024-02-02 14:04:14 +0300

    lcd-driver

---
+++ 5_lcd_driver/.Module.symvers.cmd
@@ -0,0 +1,1 @@
+cmd_/home/raspi/Linux_Driver_Dev/5_lcd_driver/Module.symvers :=  sed 's/ko$$/o/'  /home/raspi/Linux_Driver_Dev/5_lcd_driver/modules.order | scripts/mod/modpost -m -a    -o /home/raspi/Linux_Driver_Dev/5_lcd_driver/Module.symvers -e -i Module.symvers -T - 
---
+++ 5_lcd_driver/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/raspi/Linux_Driver_Dev/5_lcd_driver/modules.order := {   echo /home/raspi/Linux_Driver_Dev/5_lcd_driver/lcd_driver.ko; :; } > /home/raspi/Linux_Driver_Dev/5_lcd_driver/modules.order
